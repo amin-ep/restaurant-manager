37c7f503c8bd7712ea670f6ff0e0be4e012c8a45
Amin <amincode24@gmail.com>
2024-11-16 15:58:22 +0330

    error boundary & notfound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import Layout from "./layout/Layout";
-// import Home from "./pages/Home";
 import GlobalStyles from "./styles/GlobalStyles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
 import { AuthProvider } from "./contexts/AuthContext";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import Menu from "./pages/Menu";
-// import Orders from "./pages/Orders";
-// import Pizza from "./pages/Pizza";
 import { DarkModeProvider } from "./contexts/DarkModeContext";
-// import Order from "./pages/Order";
 import { lazy, Suspense } from "react";
 import PageLoader from "./ui/PageLoader";
-// import Account from "./pages/Account";
 
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
@@ -24,6 +17,7 @@ const Orders = lazy(() => import("./pages/Orders"));
 const Pizza = lazy(() => import("./pages/Pizza"));
 const Order = lazy(() => import("./pages/Order"));
 const Account = lazy(() => import("./pages/Account"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 function App() {
   const queryClient = new QueryClient({
@@ -59,6 +53,7 @@ function App() {
                   <Route path="account" element={<Account />} />
                 </Route>
                 <Route path="login" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
               </Routes>
             </Suspense>
           </AuthProvider>
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,99 @@
+import { Container as MUIContainer } from "@mui/material";
+import LinkButton from "../ui/LinkButton";
+import styled from "styled-components";
+
+const Container = styled(MUIContainer)`
+  display: grid;
+  grid-template-columns: auto;
+  justify-content: center;
+  margin: 6rem auto;
+  user-select: none;
+  -ms-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+`;
+
+const Span = styled.span`
+  font-size: 14rem;
+  color: var(--color-gray-8);
+  font-family: RubikWetPaint;
+
+  @media (max-width: 570px) {
+    font-size: 10rem;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 7rem;
+  }
+`;
+
+const NumberWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Img = styled.img`
+  width: 13rem;
+  height: 13rem;
+  object-fit: cover;
+
+  @media (max-width: 570px) {
+    width: 10rem;
+    height: 10rem;
+  }
+  @media (max-width: 400px) {
+    width: 7rem;
+    height: 7rem;
+  }
+`;
+
+const TextWrapper = styled.div`
+  color: var(--color-gray-8);
+  text-align: center;
+  font-family: RubikWetPaint;
+  & > h1 {
+    font-size: 4.2rem;
+    color: var(--color-green-1);
+    text-transform: uppercase;
+
+    @media (max-width: 570px) {
+      font-size: 3.2rem;
+    }
+  }
+
+  & > p {
+    font-size: 2rem;
+    @media (max-width: 570px) {
+      font-size: 1rem;
+    }
+  }
+`;
+
+const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function NotFound() {
+  return (
+    <Container maxWidth="sm">
+      <NumberWrapper>
+        <Span>4</Span>
+        <Img src="/images/circle-pizza.png" />
+        <Span>4</Span>
+      </NumberWrapper>
+      <TextWrapper>
+        <h1>Error</h1>
+        <p>Page not found!</p>
+      </TextWrapper>
+      <Actions>
+        <LinkButton to="/">Back to home</LinkButton>
+      </Actions>
+    </Container>
+  );
+}
+
+export default NotFound;
